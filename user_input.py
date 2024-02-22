1d0f8f84230828afc1d03169d53226b8ca11c2d2
bewajinis <emmanuel.bewaji@gmail.com>
2024-02-22 15:38:59 +0100

    this is my python week one assignment

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input('what is your name?  ')
+age = input('how old are you?  ')
+location = input('which location are you?  ')
+
+you = (f'Hello {name}, you are {age} years old and live in {location}. ')
+
+print(you)
